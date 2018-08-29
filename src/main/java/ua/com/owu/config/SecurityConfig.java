32edf8d6ac0895c2ddfb317241f8965ca61d890d
NazarUniyat <nazaaar50@gmail.com>
2018-08-29 14:29:29 +0300

    lold

--- src/main/java/ua/com/owu/config/SecurityConfig.java
+++ src/main/java/ua/com/owu/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package ua.com.owu.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -10,16 +9,17 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    //@Qualifier("customerServiceImpl")
+    @Qualifier("personServiceImpl")
     UserDetailsService userDetailsService;
 
     @Bean
@@ -68,4 +68,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureUrl("/error");
 
     }
+
 }
